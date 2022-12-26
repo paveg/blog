1ee474d152cb1411ac24dfa86af92484614de3b3
Ryota Ikezawa <pavegy@gmail.com>
2022-12-27 04:32:03 +0900

    :bug: Fix component

--- src/pages/privacy-policy/index.tsx
+++ src/pages/privacy-policy/index.tsx
@@ -1,9 +1,23 @@
 import { Title, Group, Text, Flex } from '@mantine/core';
 import { NextPage } from 'next';
 import Link from 'next/link';
-import React from 'react';
+import React, { FC } from 'react';
 
 const PrivacyPolicy: NextPage = () => {
+  type TextLinkProps = { children: React.ReactNode; external?: boolean; href: string };
+  const TextLink: FC<TextLinkProps> = ({ external, href, children }: TextLinkProps) => {
+    return (
+      <Text
+        color='indigo'
+        component={Link}
+        href={href}
+        rel={external ? 'noopener noreferrer' : ''}
+        target={external ? '_blank' : ''}
+      >
+        {children}
+      </Text>
+    );
+  };
   return (
     <>
       <Group mb={40} position='center'>
@@ -20,33 +34,28 @@ const PrivacyPolicy: NextPage = () => {
             当サイトはGoogle及びGoogleのパートナー（第三者配信事業者）の提供する広告を設置しております。その広告配信にはCookieを使用し、当サイトへの過去のアクセス情報に基づいて広告を配信します。
           </Text>
           <Text color='dimmed'>
-            <Text
-              color='indigo'
-              component={Link}
+            <TextLink
+              external
               href={'https://support.google.com/searchads/answer/2839090?hl=ja&ref_topic=2473095'}
             >
               DoubleClick Cookie
-            </Text>
+            </TextLink>
             を使用することにより、GoogleやGoogleのパートナーは当サイトや他のサイトへのアクセス情報に基づいて、適切な広告を当サイト上でサイト利用者に表示できます。
           </Text>
           <Text color='dimmed'>
             サイト利用者は下記のGoogleアカウントの広告設定ページで、インタレスト
             ベースでの広告掲載に使用される DoubleClick Cookie を無効にできます。また
-            <Text color='indigo' component={Link} href={'https://www.aboutads.info/'}>
+            <TextLink external href={'https://www.aboutads.info/'}>
               www.aboutads.info
-            </Text>
+            </TextLink>
             のページにアクセスして頂き、インタレスト
             ベースでの広告掲載に使用される第三者配信事業者のCookieを無効にできます。
           </Text>
           <Text color='dimmed'>
             その他、Googleの広告におけるCookieの取り扱い詳細については、
-            <Text
-              color='indigo'
-              component={Link}
-              href={'https://policies.google.com/technologies/ads?gl=jp'}
-            >
+            <TextLink external href={'https://policies.google.com/technologies/ads?gl=jp'}>
               Googleのポリシーと規約ページ
-            </Text>
+            </TextLink>
             をご覧ください。
           </Text>
         </Flex>
