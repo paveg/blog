6e937c4207494ffaf259edf8db97f10fd4e2d20d
Ryota Ikezawa <pavegy@gmail.com>
2022-12-30 04:37:09 +0900

    :thinking: Stop analytics on preview

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,33 @@
-import { createGetInitialProps } from '@mantine/next';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import nookies from 'nookies';
 import React from 'react';
+import * as gtag from '@/lib/gtag';
 
-const getInitialProps = createGetInitialProps();
+type WithNonceProps = {
+  preview: boolean;
+};
 
 // eslint-disable-next-line import/no-default-export
-export default class _Document extends Document {
-  static getInitialProps = getInitialProps;
+export default class _Document extends Document<WithNonceProps> {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const cookies = nookies.get(ctx);
+    const preview = cookies.__next_preview_data || false;
+
+    return {
+      ...initialProps,
+      preview
+    };
+  }
 
   render() {
+    const preview = this.props.preview;
+    console.info('preview: ', preview);
+
     return (
       <Html lang='ja'>
         <Head>
-          <script src='https://embed.zenn.studio/js/listen-embed-event.js' />
+          <script async src='https://embed.zenn.studio/js/listen-embed-event.js' />
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             dangerouslySetInnerHTML={{
@@ -20,8 +35,13 @@ export default class _Document extends Document {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+              ${preview ? "window['ga-disable-" + gtag.GA_TRACKING_ID + "'] = true;" : ''}
               `
             }}
+            id='gtagi-init'
           />
         </Head>
         <body>
