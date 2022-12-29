87750000017809df62f1fd06900cc78f622e4814
Ryota Ikezawa <pavegy@gmail.com>
2022-12-30 05:41:18 +0900

    :recycle: Remove export images optimization

--- next.config.js
+++ next.config.js
@@ -1,17 +1,15 @@
 /**
  * @type {import('next').NextConfig}
  *
- * @see https://zenn.dev/d_suke/articles/30925e50d5503f
- * @type {import('next-export-optimize-images').Config}
+ * @removed https://zenn.dev/d_suke/articles/30925e50d5503f
  */
 const withPlugins = require('next-compose-plugins');
-const withExportImages = require('next-export-optimize-images');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 });
 
-module.exports = withPlugins([withBundleAnalyzer, withExportImages], {
+module.exports = withPlugins([withBundleAnalyzer], {
   images: {
     domains: ['images.microcms-assets.io']
   },
