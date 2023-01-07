740dabbd07cb6366f5fef4c032225b1e0d6819c9
Ryota Ikezawa <pavegy@gmail.com>
2023-01-08 06:59:15 +0900

    :recycle: Fix

--- src/pages/sitemap.xml/index.tsx
+++ src/pages/sitemap.xml/index.tsx
@@ -8,25 +8,29 @@ import { Article } from '@/types/article';
 const generateSiteMap = (articles: Article[]) => {
   const today = FormattedTodayISODate();
   return `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
     <!-- I manually set the some URLs I know already -->
     <url>
       <loc>${siteMetadata.url}</loc>
+      <changefreq>daily</changefreq>
       <lastmod>${today}</lastmod>
       <priority>1.0</priority>
     </url>
     <url>
       <loc>${siteMetadata.url}/about-funailog</loc>
+      <changefreq>daily</changefreq>
       <lastmod>${today}</lastmod>
       <priority>1.0</priority>
     </url>
     <url>
       <loc>${siteMetadata.url}/contact</loc>
+      <changefreq>daily</changefreq>
       <lastmod>${today}</lastmod>
       <priority>1.0</priority>
     </url>
     <url>
       <loc>${siteMetadata.url}/privacy-policy</loc>
+      <changefreq>daily</changefreq>
       <lastmod>${today}</lastmod>
       <priority>1.0</priority>
     </url>
@@ -35,6 +39,7 @@ const generateSiteMap = (articles: Article[]) => {
         return `
     <url>
       <loc>${siteMetadata.url}/articles/${article.id}</loc>
+      <changefreq>weekly</changefreq>
       <lastmod>${FormattedISODate(article.revisedAt)}</lastmod>
       <priority>0.7</priority>
     </url>`;
