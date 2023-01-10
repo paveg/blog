25b79abeb4847135ebf5c1b30940047d1efe217c
Ryota Ikezawa <pavegy@gmail.com>
2023-01-10 22:12:37 +0900

    :recycle: Update share button

--- src/components/shareButtons.tsx
+++ src/components/shareButtons.tsx
@@ -1,7 +1,15 @@
 import { Center, Group } from '@mantine/core';
-import { IconBrandFacebook, IconBrandTwitter } from '@tabler/icons';
 import React, { FC } from 'react';
-import { FacebookShareButton, TwitterShareButton } from 'react-share';
+import {
+  FacebookShareButton,
+  HatenaShareButton,
+  TwitterIcon,
+  FacebookIcon,
+  TwitterShareButton,
+  HatenaIcon,
+  LineShareButton,
+  LineIcon
+} from 'react-share';
 import { siteMetadata } from '@/config/siteMetadata';
 
 type Props = {
@@ -11,18 +19,22 @@ type Props = {
 };
 
 export const ShareButtons: FC<Props> = ({ centered, articleTitle, articleUrl }) => {
+  const title = `${articleTitle} | ${siteMetadata.title}`;
+  const hashtags = [siteMetadata.title];
   const shares = (
     <Group mb={20}>
-      <TwitterShareButton
-        hashtags={[siteMetadata.title]}
-        title={`${articleTitle} | ${siteMetadata.title}`}
-        url={articleUrl}
-      >
-        <IconBrandTwitter />
+      <TwitterShareButton hashtags={hashtags} title={title} url={articleUrl}>
+        <TwitterIcon round size={28} />
       </TwitterShareButton>
-      <FacebookShareButton hashtag={siteMetadata.title} title={articleTitle} url={articleUrl}>
-        <IconBrandFacebook />
+      <FacebookShareButton hashtag={siteMetadata.title} title={title} url={articleUrl}>
+        <FacebookIcon round size={28} />
       </FacebookShareButton>
+      <HatenaShareButton title={title} url={articleUrl}>
+        <HatenaIcon round size={28} />
+      </HatenaShareButton>
+      <LineShareButton title={title} url={articleUrl}>
+        <LineIcon round size={28} />
+      </LineShareButton>
     </Group>
   );
   return <>{centered ? <Center>{shares}</Center> : shares}</>;
