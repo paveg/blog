d818ff7ea1acb3171aa95491262c69a3c5f209ef
Ryota Ikezawa <pavegy@gmail.com>
2023-01-31 01:47:07 +0900

    :recycle: Fix

--- src/pages/about-funailog/index.tsx
+++ src/pages/about-funailog/index.tsx
@@ -22,7 +22,7 @@ const AboutFunailog: NextPage = () => {
     return age;
   };
 
-  const realAge = calculateAge(birthday);
+  const realAge = calculateAge();
   const text1 =
     'フナイログは、『ガジェット』を中心に、PCやデスク周辺の『仕事道具』や『乗り物のコト』を綴る個人メディアです。買ってよかったモノなど、フナイログの中の人の目線で紹介しています。';
   return (
